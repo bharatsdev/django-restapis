c79cfc412dce7f5fa60eef201127cca59c4ca814
everythingisdata <bharatmca2010@gmail.com>
2021-02-15 07:19:01 +0530

    Custom User model added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # restapis-django-basics
+
+
+    1- vagrant up
+    2- vagrant ssh (login to vagrant)
+    3- python -m venv ~/env (create virtual env)
+    source ~/env/bin/activate
+    deactviate
+    
+    Activate irtual env and pip install command V
+    pip  freeze>requirements.txt
---
+++ profile_api/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class UserProfilerManager(BaseUserManager):
+    """Manager for mange user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError("User must have a email address..")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.save(using=self.db)
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    # is_ = models.BooleanField(default=False)
+
+    objects = UserProfilerManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrieve full name of the user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve short name of the user"""
+        return self.name
+
+    def __str__(self):
+        """Return String representation of our user"""
+        return self.email
